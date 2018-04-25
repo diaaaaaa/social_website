99ae734b408fd7586ecb246eb8963a2eed68c11b
diaaaaaa <ite.diaa@gmail.com>
2018-04-25 16:17:14 +0200

    initializing Web + MongoDB + Thymeleaf

---
+++ src/main/java/at/refugeescode/social_website/SocialWebsiteApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.social_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialWebsiteApplication.class, args);
+	}
+}
